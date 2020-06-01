38aec3347920291f3782b7fab91175e64f7eb298
mamtasonar1127 <46754967+mamtasonar1127@users.noreply.github.com>
2020-06-01 12:55:46 -0400

    class components and function components

---
+++ src/home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default class Home extends React.Component{
+
+    render()
+    {
+        return(
+            <div>
+                <h1>home component</h1>
+                <h3>sub</h3>
+            </div>
+        )
+    }
+}
