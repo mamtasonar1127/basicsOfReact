edae40b1fae85c6782e9baedc28c6e9138d0b386
mamtasonar1127 <46754967+mamtasonar1127@users.noreply.github.com>
2020-06-01 15:53:49 -0400

    props with class components and function components examples

--- src/App.js
+++ src/App.js
@@ -7,29 +7,12 @@ import Profile from './Profile'
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
+       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-         Component 
-        </a> 
-
-      <Home/>
-      <Profile text="Function Profile Component"/>
-
-      </header> */}
-      {
-          React.createElement(
-          'h1',
-          null,
-          'Hello JSX'
-        )
-      }
-
+        
+       <Profile text={{name:'mamta'}}/>
+      </header>
+      
     </div>
   );
 }
--- src/Profile.js
+++ src/Profile.js
@@ -19,15 +19,35 @@ import React from 'react'
 
 //Fuction Component
 
+// export default function Profile(prop){
 
+//     //arrow function
+//     const red = () =>{
+//         alert("red function called")
+//     }
+//     return <div>
+//       <h1 onClick={red}>{prop.text}</h1>  
+//     </div>
+// }
 
-export default function Profile(prop){
+//Props with functional components. 
 
-    //arrow function
-    const red = () =>{
-        alert("red function called")
+// export default function Profile(prop){
+//     return(
+//         <div>
+//             <h1>{prop.text.name}</h1>
+//         </div>
+//     )
+// }
+
+//Props with class components.
+export default class Profile extends React.Component{
+    render()
+    {
+        return(
+            <div>
+                <h1>{this.props.text.name}</h1>
+            </div>
+        )
     }
-    return <div>
-      <h1 onClick={red}>{prop.text}</h1>  
-    </div>
-}
+}   
